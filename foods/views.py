bc246faa47a98dc27a71732a4b3bb5e1535b79dc
kmswlee <wodndl895@kookmin.ac.kr>
2021-05-08 14:38:44 +0900

    first commit

---
+++ foods/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from datetime import datetime
+from django.http import HttpResponse
+from django.http import Http404
+
+# Create your views here.
+
+def index(request):
+    today = datetime.today()
+    context = {"date": today}
+    return render(request,'foods/index.html', context = context)
+
+def food_detail(request,food):
+    context = dict()
+    if food == 'chicken':
+        context["name"] = "코딩에 빠진 닭"
+        context["description"] = "주머니가 가벼운 당신의 마음까지 생각한 가격!"
+        context['price'] = 10000
+        context['img_path'] = 'foods/images/chicken.jpg'
+    else:
+        raise Http404("이런맛 없다!")
+    return render(request,'foods/detail.html',context)
